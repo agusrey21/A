4354a71cbab097a2f0cb49908c86f59fa0d785a1
Rey Agustin <agustinrey419@gmail.com>
2024-10-07 23:55:15 +0000

    puntob

--- parcial1/puntob.sh
+++ parcial1/puntob.sh
@@ -1,1 +1,25 @@
-vagrant
+#dividir el disco
+
+sudo fdisk /dev/sdd
+lsblk
+
+#formatear las particiones
+
+sudo mkfs.ext4 /dev/sdd1
+sudo mkfs.ext4 /dev/sdd2
+sudo mkfs.ext4 /dev/sdd3
+sudo mkfs.ext4 /dev/sdd4
+
+#montar en /mnt/
+
+ll /mnt/
+sudo mkdir /mnt/sdd1
+sudo mount /dev/sdd1 /mnt/sdd1
+ll /mnt/
+sudo mkdir /mnt/sdd2
+sudo mount /dev/sdd2 /mnt/sdd2
+sudo mkdir /mnt/sdd3
+sudo mount /dev/sdd3 /mnt/sdd3
+sudo mkdir /mnt/sdd4
+sudo mount /dev/sdd4 /mnt/sdd4
+ll /mnt/
