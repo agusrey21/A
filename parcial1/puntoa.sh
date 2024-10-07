715fd5834df1d9893783ce9889b3656e01b63f9e
Rey Agustin <agustinrey419@gmail.com>
2024-10-07 21:46:24 +0000

    primer commit: Agregando archivos bash

---
+++ parcial1/puntoa.sh
@@ -0,0 +1,3 @@
+mkdir -p Examen-UTN/{alumno_{1..3}/{parcial_{1..3}},profesor}
+tree Examenes-UTN
+
---
+++ parcial1/puntoc.sh
@@ -0,0 +1,8 @@
+sudo useradd developer1
+sudo useradd tester1
+sudo useradd devops1
+sudo useradd devops2
+cat /etc/passwd |tail
+sudo grupadd grupodeveloper
+sudo grupadd grupodevops
+sudo grupadd grupotester
