1440c531c09b220fbc1c3bb7c3119893fa35e42b
Rey Agustin <agustinrey419@gmail.com>
2024-10-08 01:57:32 +0000

    punto f

--- parcial1/puntof.sh
+++ parcial1/puntof.sh
@@ -1,1 +1,6 @@
-vagrant
+cat << EOF |tee $FILTROAVANZADO.txt
+Mi IP Publica es: $(curl -s ifconfig.me)
+Mi Usuario es: $(whoami)
+El Hash de mi Usuario es: $(sudo cat /etc/shadow |grep vagrant |awk -F ':' '{print $2}')
+
+EOF
