8fb138e136ac5d905765ad0642c84a31ec548026
Rey Agustin <agustinrey419@gmail.com>
2024-10-08 01:41:07 +0000

    punto e

--- parcial1/puntoe.sh
+++ parcial1/puntoe.sh
@@ -1,1 +1,11 @@
-vagrant
+#busco en /proc la memoria total y la sobreescribo en filtro_basico
+
+cat /proc/meminfo |grep MemTotal > filtro_basico.txt
+cat filtro_basico.txt
+
+#agrego un texto 
+echo 'Chassis Information' >> filtro_basico.txt
+
+#envio al final del archivo la informacion del fabricante del chassis
+sudo dmidecode -t chassis |grep Manufacturer >> filtro_basico.txt
+cat filtro_basico.txt
